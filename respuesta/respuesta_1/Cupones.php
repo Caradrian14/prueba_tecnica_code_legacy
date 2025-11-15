2e087dace0c59b85745b4d7f6c7c9429bc80559d
Adria <adria.jorda.m@gmail.com>
2025-11-15 12:48:11 +0100

    [add] obj cupones

--- respuesta/respuesta_1/Cupones.php
+++ respuesta/respuesta_1/Cupones.php
@@ -1,7 +1,20 @@
 <?php
 
 class Cupones {
-    public function __construct($sku, $nombre, $precio, $etiquetas = []) {
-    
+    public $id
+    public $name;
+    public $start_date;
+    public $finish_date;
+
+    public function __construct($name,$start_date, $finish_date) {
+        $this->name = $name;
+        $this->start_date = $start_date;
+        $this->finish_date = $finish_date;
     }
+
+    // Como añadir reglas de negocio? 
+    // cada cupon una funcion <----
+    // integrarlo en el array general? demasiado complejo y usran ddbb
+
+    //SOLUCION: crear objetos cupones hijos para subdividir la responsabilidad del negocio 
 }
