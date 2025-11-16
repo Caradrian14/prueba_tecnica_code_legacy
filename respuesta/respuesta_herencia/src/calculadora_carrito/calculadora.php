b9a85dfd959f39f8a6a478923b80723c8ae56ed8
Adria <adria.jorda.m@gmail.com>
2025-11-16 10:32:14 +0100

    [add] objeto general de calculadora

--- respuesta/respuesta_herencia/src/calculadora_carrito/calculadora.php
+++ respuesta/respuesta_herencia/src/calculadora_carrito/calculadora.php
@@ -1,4 +1,20 @@
 <?php
 
+namespace respuesta_herencia\src\cupones;
+
 //Aqui ira logica del negocio 
 //como si fuera el "calculate_legacy_price" del script legalicy
+
+class Calculadora {
+    private $carrito;
+    private $cupones;
+
+    public function __construct($carrito, $cupones) {
+        $this->carrito = $carrito;
+        $this->cupones = $cupones;
+    }
+
+
+    // Vamos a empezar con que las reglas de negocio son funciones, una vez ya funcional las pasamos a objetos.
+    
+}
