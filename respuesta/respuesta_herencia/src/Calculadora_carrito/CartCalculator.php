54ee3ff4852245111560023f220561ab22f7d02c
Adria <adria.jorda.m@gmail.com>
2025-11-18 10:16:24 +0100

    [change] nombre de varia svariabes menores a ingles

--- respuesta/respuesta_herencia/src/Calculadora_carrito/CartCalculator.php
+++ respuesta/respuesta_herencia/src/Calculadora_carrito/CartCalculator.php
@@ -21,22 +21,22 @@ class CartCalculator {
     // Pasar a un fctory?
     public function object_creation($cart, $coupons) {
         // Usamos el array a modoo de bbdd pero aqui habria que hacer una consulta select where a la bbdd 
-        $PRODUCTOS_DB = require __DIR__ . "/../../datos/productos.php";
+        $PRODUCTS_DB = require __DIR__ . "/../../datos/productos.php";
         $COUPONS_DB = require __DIR__ . "/../../datos/cupones.php";
-        foreach ($cart as $item_carrito) {
-            if (!isset($PRODUCTOS_DB[$item_carrito['sku']])) {
+        foreach ($cart as $item_cart) {
+            if (!isset($PRODUCTS_DB[$item_cart['sku']])) {
                 continue; // Ignorar productos que no existen
             }
 
-            $sku = $item_carrito['sku'];
-            $name  = $PRODUCTOS_DB[$sku]['name']  ?? '';
-            $quantity  = $item_carrito['quantity']  ?? '';
-            $price = $PRODUCTOS_DB[$sku]['price'] ?? 0;
-            $tag   = $PRODUCTOS_DB[$sku]['tag']   ?? [];
-            $items_de_producto =["sku" => $sku, "name" => $name, "quantity" => $quantity, "price"=>$price, "tag"=>$tag];
-            $producto_carrito = ProductoFactory::fromArray($items_de_producto);
+            $sku = $item_cart['sku'];
+            $name  = $PRODUCTS_DB[$sku]['name']  ?? '';
+            $quantity  = $item_cart['quantity']  ?? '';
+            $price = $PRODUCTS_DB[$sku]['price'] ?? 0;
+            $tag   = $PRODUCTS_DB[$sku]['tag']   ?? [];
+            $items_from_product =["sku" => $sku, "name" => $name, "quantity" => $quantity, "price"=>$price, "tag"=>$tag];
+            $product_cart = ProductoFactory::fromArray($items_from_product);
             // Asignar los valores correspondientes del array a su variable
-            $this->carrito[] = $producto_carrito;
+            $this->cart[] = $product_cart;
         }
 
         foreach ($coupons as $item_cupon) {
@@ -48,9 +48,9 @@ class CartCalculator {
             $finish_date   = $COUPONS_DB[$name][1]; // revisar para qu eno haya fallos
             $acumulative = $COUPONS_DB[$name][2] ?? true; // acumulativos
             // Asignar los valores correspondientes del array a su variable
-            $items_de_cupon =["name" => $name, "start_date" => $start_date, "finish_date"=>$finish_date, "acumulative"=>$acumulative];
+            $items_from_coupon =["name" => $name, "start_date" => $start_date, "finish_date"=>$finish_date, "acumulative"=>$acumulative];
             
-            $cupon_carrito = CuponFactory::fromArray($items_de_cupon);
+            $cupon_carrito = CuponFactory::fromArray($items_from_coupon);
             $this->coupons[] = $cupon_carrito;
         }
     }
@@ -61,18 +61,18 @@ class CartCalculator {
         $bogo_discount = 0;  // Descuento acumulado por BOGO
 
         //Obtenemos los datos de la bbdd
-        $PRODUCTOS_DB = require __DIR__ . "/../../datos/productos.php";
+        $PRODUCTS_DB = require __DIR__ . "/../../datos/productos.php";
 
         //1. Calcular subtotal bruto
-        foreach ($this->carrito as $item) {
+        foreach ($this->cart as $item) {
             $sku = $item->getSku();
             $quantity = $item->getQuantity();
             //Hay que comprobar que los productos existan ( esto seria un select where en la bbdd)
-            if (!isset($PRODUCTOS_DB[$sku])) {
+            if (!isset($PRODUCTS_DB[$sku])) {
                 continue; // Ignorar productos que no existen
             }
     
-            $product_data = $PRODUCTOS_DB[$sku];
+            $product_data = $PRODUCTS_DB[$sku];
             $price = $product_data['price'];
     
             // Acumular subtotal bruto y total de items
